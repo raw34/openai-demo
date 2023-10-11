d4fb0b9181cac87de24530ddc77aedf83870c46d
raw34 <raw0034@gmail.com>
2023-10-11 12:56:20 +0800

    feat: 实现 gpt 模型调用的方法

--- src/main.py
+++ src/main.py
@@ -1,6 +1,47 @@
-def greet():
-    return "Hello, welcome to the project!"
+import openai
+import configparser
+
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+    return config['openai']['api_key']
+
+
+def generate_text(model_name, prompt):
+    openai.api_key = get_api_key()
+    response = openai.Completion.create(
+        engine=model_name,
+        prompt=prompt,
+        temperature=0.7,
+        max_tokens=50
+    )
+    return response
+
+
+def generate_text_chat(model_name, messages):
+    openai.api_key = get_api_key()
+    response = openai.ChatCompletion.create(
+        model=model_name,
+        messages=messages,
+        temperature=0.7,
+        max_tokens=1024
+    )
+    return response
 
 
 if __name__ == "__main__":
-    print(greet())
+    # model_name = "davinci"
+    # prompt = "Translate the following English text to French: 'Hello, how are you?'"
+    # print(generate_text(model_name, prompt))
+
+    # model_name = "gpt-3.5-turbo"
+    model_name = "gpt-4"
+    prompt = "As an intelligent AI model, if you could be any fictional character, who would you choose and why?"
+    messages = [
+        {
+            "role": "user",
+            "content": prompt,
+        },
+    ]
+    print(generate_text_chat(model_name, messages))
